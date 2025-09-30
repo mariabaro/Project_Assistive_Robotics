107ee6e06c5e6bebd99c6b46b810810bf984ba82
berta-haro <bharojov7@alumnes.ub.edu>
2025-09-30 11:15:11 +0200

    python code arreglat pel robot

--- src/python_scripts/Assistive_hand_SW_HW_sockets_Berta2.py
+++ src/python_scripts/Assistive_hand_SW_HW_sockets_Berta2.py
@@ -9,7 +9,7 @@ from robodk.robomath import *   # Funciones de matemáticas de RoboDK
 # ------------------------------
 # Cargar el proyecto RoboDK
 # ------------------------------
-relative_path = "src/roboDK/Assistive_UR5e_Maria.rdk"
+relative_path = "Project_Assistive_Robotics/src/roboDK/Assistive_UR5e_Maria.rdk"
 absolute_path = os.path.abspath(relative_path)
 RDK = Robolink()
 RDK.AddFile(absolute_path)
@@ -33,7 +33,12 @@ pregrasp = RDK.Item("PreGrasp")
 grasp = RDK.Item("Grasp")
 pregive = RDK.Item("PreGive")
 give = RDK.Item("Give")
-init_joints = list(np.radians(init.joints())[0])
+init_joints = list(np.radians(init.Joints())[0])
+pregrasp_joints = list(np.radians(pregrasp.Joints())[0])
+grasp_joints = list(np.radians(grasp.Joints())[0])
+pregive_joints = list(np.radians(pregive.Joints())[0])
+give_joints = list(np.radians(give.Joints())[0])
+
 # ------------------------------
 # Conexión al robot real por socket
 # ------------------------------
@@ -66,27 +71,51 @@ def go_init():
     print("Going to INIT")
     robot.MoveJ(init, True)
     if robot_is_connected:
-        cmd = f"movej([{init_joints}], {accel_mss}, {speed_ms},{timel},0)"
+        cmd = f"movej([{','.join(map(str, init_joints))}], {accel_mss}, {speed_ms},{timel},0)"
         send_ur_script(cmd)
         receive_response(timel)
 
 def pick_object():
     print("Going to PreGrasp")
     robot.MoveL(pregrasp, True)
+    if robot_is_connected:
+        cmd = f"movel([{','.join(map(str, pregrasp_joints))}], {accel_mss}, {speed_ms},{timel},0)"
+        send_ur_script(cmd)
+        receive_response(timel)
+
     robot.MoveL(grasp, True)
+    if robot_is_connected:
+        cmd = f"movel([{','.join(map(str, grasp_joints))}], {accel_mss}, {speed_ms},{timel},0)"
+        send_ur_script(cmd)
+        receive_response(timel)
     print("Grasp done (simulación: cerrar gripper)")
-    # aquí comando al gripper real si lo tienes
     time.sleep(2)  # ⏸ pausa de 3 segundos después del Grasp
     robot.MoveL(pregrasp, True)
+    if robot_is_connected:
+        cmd = f"movel([{','.join(map(str, pregrasp_joints))}], {accel_mss}, {speed_ms},{timel},0)"
+        send_ur_script(cmd)
+        receive_response(timel)
+
 
 def give_object():
     print("Giving object")
     robot.MoveL(pregive, True)
+    if robot_is_connected:
+        cmd = f"movel([{','.join(map(str, pregive_joints))}], {accel_mss}, {speed_ms},{timel},0)"
+        send_ur_script(cmd)
+        receive_response(timel)
     robot.MoveL(give, True)
+    if robot_is_connected:
+        cmd = f"movel([{','.join(map(str, give_joints))}], {accel_mss}, {speed_ms},{timel},0)"
+        send_ur_script(cmd)
+        receive_response(timel)
     print("Give done")
-    # aquí comando de abrir gripper si procede
     time.sleep(2)  # ⏸ pausa de 3 segundos después del Give
     robot.MoveL(pregive, True)
+    if robot_is_connected:
+        cmd = f"movel([{','.join(map(str, pregive_joints))}], {accel_mss}, {speed_ms},{timel},0)"
+        send_ur_script(cmd)
+        receive_response(timel)
 
 # ------------------------------
 # Main
